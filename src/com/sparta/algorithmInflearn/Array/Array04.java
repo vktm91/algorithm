3263e6d2703ef2cb9c9d4eb8336f6be801626769
vktm91 <vktm91@nate.com>
2021-12-28 22:22:02 +0900

    피보나치 수열

---
+++ src/com/sparta/algorithmInflearn/Array/Array04.java
@@ -0,0 +1,22 @@
+package com.sparta.algorithmInflearn.Array;
+
+import java.util.Scanner;
+
+// 피보나치 수열
+public class Array04 {
+    public int[] solution(int n){
+        int[] answer=new int[n];
+        answer[0]=1;
+        answer[1]=1;
+        for(int i=2; i<n; i++){
+            answer[i]=answer[i-2]+answer[i-1];
+        }
+                return answer;
+    }
+    public static void main(String[] args){
+        Array04 T = new Array04();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        for(int x:T.solution(n)) System.out.println(x+" ");
+    }
+}
