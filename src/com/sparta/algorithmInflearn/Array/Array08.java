c9bc68589210fd46e58691c7cacff7d9331c95a3
vktm91 <vktm91@nate.com>
2022-01-03 14:52:25 +0900

    등수구하기

---
+++ src/com/sparta/algorithmInflearn/Array/Array08.java
@@ -0,0 +1,30 @@
+package com.sparta.algorithmInflearn.Array;
+
+import java.util.Scanner;
+
+// 등수구하기
+public class Array08 {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Array08 T = new Array08();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : T.solution(n, arr)) System.out.print(x + " ");
+    }
+
+}
