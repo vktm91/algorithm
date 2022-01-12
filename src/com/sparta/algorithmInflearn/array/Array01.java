b2ed87ef7cff670bbb57ec5795777d1d9c3c7539
vktm91 <vktm91@nate.com>
2022-01-12 15:26:49 +0900

    큰수 출력하기

--- src/com/sparta/algorithmInflearn/array/Array01.java
+++ src/com/sparta/algorithmInflearn/array/Array01.java
@@ -3,7 +3,7 @@ package com.sparta.algorithmInflearn.array;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// 큰 수 출력하기
+// 큰수 출력하기
 public class Array01 {
     public ArrayList<Integer> solution(int n, int[] arr) {
         ArrayList<Integer> answer = new ArrayList<>();
