9cf1fe383ab866c4b5faf0b832e5a5f4efa2409e
vktm91 <vktm91@nate.com>
2024-11-29 19:13:20 +0900

    baekjoon 11720 2nd try

--- src/main/java/com/sparta/baekjoon/arrayAndList/Array11720.java
+++ src/main/java/com/sparta/baekjoon/arrayAndList/Array11720.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 // Baekjoon No.11720
 public class Array11720 {
     public static void main(String[] args) {
-        firstTry();
+//        firstTry();
+        secondTry();
     }
 
     private static void firstTry() {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
         int target = kb.nextInt();
+
         int sum = 0;
 
         for (int i = n; i > 0; i--) {
@@ -26,4 +28,23 @@ public class Array11720 {
 
         System.out.println(sum);
     }
+
+    private static void secondTry() {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String sTarget = kb.next();
+
+        int sum = 0;
+
+        char[] charArray = sTarget.toCharArray();
+
+        for (int i = 0; i < charArray.length; i++) {
+            int num = charArray[i] - '0';
+
+            sum += num;
+        }
+
+        System.out.println(sum);
+    }
+
 }
